f7e23f3298429670376d9b50daf02dc4cfdfa8f3
Mathieu MOULLEC <mmoullec@e1r11p16.42.fr>
2018-04-16 17:32:52 +0200

    finish login

--- react/src/components/pages/LoginPage.tsx
+++ react/src/components/pages/LoginPage.tsx
@@ -2,10 +2,17 @@ import * as React from "react";
 
 import LoginForm from "../forms/LoginForm";
 
-const LoginPage = () => (
-    <div>
-        <h1>Page de Login</ h1>
-        <LoginForm/>
-    </div>
-);
+class LoginPage extends React.Component {
+    public submit = (data) => {
+        window.console.log(data);
+    };
+
+    public render = () =>
+        (<div>
+            <h1>Page de Login</ h1>
+            <LoginForm submit={this.submit}/>
+        </div>)
+
+}
+
 export default LoginPage;
